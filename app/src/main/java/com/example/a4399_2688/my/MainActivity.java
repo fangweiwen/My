86cfe94e656b2d6559c40c3217033409b233bb17
fangweiwen <fangweiwen@4399inc.com>
2019-03-13 14:14:08 +0800

    test1 Initial commit

---
+++ app/src/main/java/com/example/a4399_2688/my/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4399_2688.my;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //123
+}
